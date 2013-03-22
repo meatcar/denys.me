7db212c66e826ec1b3e99b9eef0ed0f325d2dab1
Denys Pavlov <denys.pavlov@gmail.com>
2013-03-21 23:21:39 -0400

    added dynamic borders, added color contrast

---
+++ js/dynamic_borders.js
@@ -0,0 +1,52 @@
+(function (app) {
+  app($, _, this.window, undefined);
+}(function ($, _, window){
+  /*
+   * Returns a uniformly random number between min and max
+   */
+  function rand(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function rand_point() {
+    var x = rand(min, max),
+        y = rand(min, max);
+    if (x < y) {
+      x = x/2;
+    } else {
+      y = y/2;
+    }
+    return {x: x, y: y};
+  }
+
+  var min = 0,
+      max,
+      // Direction Indeces
+      dirs = {
+          right: 1,
+          bottom: 0,
+          top: 2,  // top and bottom indeces swapped
+          left: 3
+      };
+
+  $(document).ready(function () {
+    // get max width out of css
+    max = $('.wrapper').first().css('width');
+    max = parseInt(max, 10);
+    _.each($('.triangle'), size_corner);
+  });
+
+  /**
+   * Resize the corner given the vertical orientation and element (top, $('triangle'))
+   */
+  function size_corner(el) {
+    var $el = $(el),
+        v = $el.parent().hasClass('top') ? 'top' : 'bottom',
+        h = $el.hasClass('right') ? 'right' : 'left',
+        width = [0, 0, 0, 0],
+        point = rand_point();
+    width[dirs[v]] = point.y + "px";
+    width[dirs[h]] = point.x + "px";
+    $el.css('border-width', width.join(' '));
+  }
+}));
