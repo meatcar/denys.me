413bc1c603cd152bdd3b22ecf7fce14de3248933
Denys Pavlov <me@denys.me>
2020-11-20 01:51:10 -0500

    remove jquery, lowdash dep, switch to feather icons, add gif headshot

--- js/dynamic_borders.js
+++ js/dynamic_borders.js
@@ -1,60 +1,53 @@
-(function (app) {
-  app($, _, window, undefined);
-}(function ($, _, window){
+var // Direction Indeces
+    dirs = {
+        right: 1,
+        bottom: 0,
+        top: 2,  // top and bottom indeces swapped
+        left: 3
+    };
 
-  /*
-   * Returns a uniformly random number between min and max
-   */
-  function rand(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+/*
+ * Returns a uniformly random number between min and max
+ */
+function rand(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function rand_point() {
+  var x = rand(max),
+      y = rand(max);
+  if (x < y) {
+    x = x/3;
+  } else {
+    y = y/3;
   }
+  return {x: x, y: y};
+}
 
-  function rand_point() {
-    var x = rand(min, max),
-        y = rand(min, max);
-    if (x < y) {
-      x = x/2;
-    } else {
-      y = y/2;
-    }
-    return {x: x, y: y};
-  }
-
-  /**
-   * Resize the corner given the vertical orientation and element
-   * (top, $('triangle'))
-   */
-  function size_corner(dirs, min, max, el) {
-    var $el = $(el),
-        v = $el.parent().hasClass('top') ? 'top' : 'bottom',
-        h = $el.hasClass('right') ? 'right' : 'left',
-        width = [0, 0, 0, 0],
-        point = rand_point();
-    width[dirs[v]] = point.y + "px";
-    width[dirs[h]] = point.x + "px";
-    $el.css('border-width', width.join(' '));
-  }
+/**
+ * Resize the corner given the vertical orientation and element
+ * (top, $('triangle'))
+ */
+function size_corner(dirs,  max, el) {
+  var v = el.parentNode.classList.contains('top') ? 'top' : 'bottom',
+      h = el.classList.contains('right') ? 'right' : 'left',
+      width = [0, 0, 0, 0],
+      point = rand_point(max);
+  width[dirs[v]] = point.y + "px";
+  width[dirs[h]] = point.x + "px";
+  el.style.borderWidth = width.join(' ');
+}
 
 
-  var min = 0,
-      max,
-      // Direction Indeces
-      dirs = {
-          right: 1,
-          bottom: 0,
-          top: 2,  // top and bottom indeces swapped
-          left: 3
-      };
 
-  $(document).ready(function () {
-    // get max width out of css
-    max = $('.wrapper').first().css('width');
-    max = parseInt(max, 10);
-    _.each($('.triangle'), _.partial(size_corner, dirs, min, max));
+window.addEventListener('load', function (event) {
+  feather.replace()
+  document.querySelector('.content').style.display = 'block';
+
+  // get max width out of css
+  max = document.querySelector('.wrapper').getBoundingClientRect().width;
+  max = parseInt(max, 10);
+  document.querySelectorAll('.triangle').forEach(function (e) {
+    size_corner(dirs, max, e);
   });
-
-  $(window).load(function (){
-    $('.content').show();
-  });
-
-}));
+});
